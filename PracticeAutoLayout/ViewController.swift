e92dddea8d836b2bdb8b9459d41e520f274306da
Taehwan Kim <obtusa07@gmail.com>
2022-12-24 20:00:33 +0900

    Initial Commit

---
+++ PracticeAutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeAutoLayout
+//
+//  Created by Taehwan Kim on 2022/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
