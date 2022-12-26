1bddfaf5628d7e6535481728e7b22aa9b9914d40
Taehwan Kim <obtusa07@gmail.com>
2022-12-26 21:10:46 +0900

    :sparkles: add RxSwift

--- PracticeAutoLayout/ViewController.swift
+++ PracticeAutoLayout/ViewController.swift
@@ -9,12 +9,18 @@ import UIKit
 import SnapKit
 import Then
 import Combine
+import RxCocoa
+import RxSwift
 
 class ViewController: UIViewController {
+    // combine
     @Published var loadingState: LoadingButton.LoadingState = .normal
-    // 뷰 컨트롤러가 메모리에서 해제될 때 컴바인도 같이 해제되어야 한다.
-    
     var subscriptions = Set<AnyCancellable>()
+    
+    // rx
+    let loadingStateRx = BehaviorRelay<LoadingButton.LoadingState>(value: .normal)
+    var disposeBag = DisposeBag()
+    
     lazy var myScrollView: UIScrollView = UIScrollView().then {
         // 스크롤바 바운싱 설정
         $0.isUserInteractionEnabled = true
@@ -62,15 +68,46 @@ class ViewController: UIViewController {
         dummyButtons.forEach {
             butttonStackView.addArrangedSubview($0)
             $0.addTarget(self, action: #selector(onButtonClicked(_:)), for: .touchUpInside)
+            // MARK: - Combine
             // published 된 요소를 사용하고자 하면 앞에 $사인을 붙인다.
             // on은 붙이고자 하는 객체. 이 경우에는 dummyButtons들이다.
             // to는 on에서 붙은 객체의 속성들이다.
             // combine에서는 메모리 관리를 해줘야 한다.
             // 콤바인 퍼플리셔 데이터 상태 <-> 버튼의 loadingState
-            self.$loadingState
-                .assign(to: \.loadingState, on: $0)
-                .store(in: &subscriptions)
+//            self.$loadingState
+//                .assign(to: \.loadingState, on: $0)
+//                .store(in: &subscriptions)
+            
+            // MARK: - Rx
+            // Rx 옵저버블 데이터 상태 <-> 버튼의 loadingState
+            self.loadingStateRx
+                .bind(to: $0.rx.loadingState)
+                .disposed(by: disposeBag)
+            
         }
+        let loadingStateLabel = UILabel().then {
+            $0.text = "로딩 상태 레이블"
+            $0.font = UIFont.systemFont(ofSize: 30)
+            $0.backgroundColor = .white
+        }
+        self.view.addSubview(loadingStateLabel)
+        loadingStateLabel.snp.makeConstraints{
+            $0.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
+            $0.centerX.equalToSuperview()
+        }
+        
+        // combine
+        self.$loadingState
+            .map { $0 == .loading ? "로딩중" : "일반" }
+            .assign(to: \.text, on: loadingStateLabel)
+            .store(in: &subscriptions)
+        
+        // Rx
+        self.loadingStateRx
+            .map { $0 == .loading ? "로딩중" : "일반" }
+            .bind(to: loadingStateLabel.rx.text)
+            .disposed(by: disposeBag)
+        
     }
 
 }
@@ -84,13 +121,25 @@ extension ViewController {
         
         // 만약 이 버튼이 로그인 버튼이라면 한번 눌리면 못 누르게 해야함
         // 아래 코드로 눌렸다는 것이 감지되면 return으로 함수를 끝내서 다시 눌리는걸 예방할 수 있음. 혹은 로딩 버튼에서 isUserInteractionEnabled를 비활성화 하는 방법으로도 막아놨음
-        if self.loadingState == .loading {
+        // MARK: - combine
+//        if self.loadingState == .loading {
+//            return
+//        }
+//        self.loadingState = .loading
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
+//            self.loadingState = .normal
+//        })
+        // MARK: - Rx
+        
+        if self.loadingStateRx.value == .loading {
             return
         }
-        self.loadingState = .loading
+        
+        self.loadingStateRx.accept(.loading)
         DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {
-            self.loadingState = .normal
+            self.loadingStateRx.accept(.normal)
         })
+        
         // combine의 publisher 데이터 상태를 변경
 //        if self.loadingState == .normal {
 //            self.loadingState = .loading
